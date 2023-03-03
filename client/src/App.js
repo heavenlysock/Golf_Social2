2273a04d9a5bbf4019d0ed3ce99e42432049a0ff
Robert Lee <bobertsworld@gmail.com>
2023-03-03 12:45:27 -0800

    fixed config in wrong direct

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
+import { Routes , Route } from 'react-router-dom'
+import Main from './components/Main';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import UserDetail from './components/UserDetail';
+import CourseDetail from './components/CourseDetail';
+import CourseList from './components/CourseList';
+import ReviewList from './components/ReviewList';
+import UserList from './components/UserList';
+import NavBar from './components/NavBar';
+import React from 'react';
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+  console.log(currentUser)
+  const [displayInfo, setDisplayInfo] = useState(null)
+
+  
+
+
+  useEffect(() => {
+    fetch('/me')
+      .then(response => {
+        if(response.ok) {
+          response.json()
+          .then((user) => setCurrentUser(user))
+        } else{
+          setCurrentUser(null)
+        }
+      })
+
+  }, [])
+
+
+  function onLogIn(loggedInUser) {
+    setCurrentUser(loggedInUser)
+
+  }
+
+  function onLogOut(){
+    setCurrentUser(null)
+  }
+
+  function onShowDetails(individual) {
+    setDisplayInfo(individual)
+  }
+
+  function onDeleteUser() {
+    setCurrentUser(null)
+    setDisplayInfo(null)
+  }
+
+  if(!currentUser) return (
+    <Routes>
+      {/* <Route path='/' element={<Welcome/>}/> */}
+      <Route exact path='/' element={<Login onLogIn={onLogIn} setCurrentUser={setCurrentUser}  />} />
+      <Route exact path='/signup' element={<Signup onLogIn={onLogIn} />} />
+    </Routes>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar
+        currentUser={currentUser}
+        onLogOut={onLogOut}
+        />
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+
+          <Route exact path="/reviews" element={<ReviewList />} />
+
+          <Route exact path="/users" element={<UserList />} />
+
+
+          <Route exact path="/courses" element={<CourseList />} />
+
+          <Route exact path='/courses/:id' element={<CourseDetail onShowDetails={onShowDetails} displayInfo={displayInfo} currentUser={currentUser} onDeleteUser={onDeleteUser} />} />
+
+          <Route exact path='/users/:id' element={<UserDetail onShowDetails={onShowDetails} displayInfo={displayInfo} currentUser={currentUser} onDeleteUser={onDeleteUser} />} />
+
+          
+        </Routes>
+      
     </div>
   );
 }
---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  # get '/courses', to: 'courses#render_courses'
+
+  post '/signup', to: 'users#create'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/me', to: 'users#show'
+
+
+
+  resources :reviews
+  resources :courses, only: [:index, :show, :create, :update]
+  resources :users, except: [:create, :show]
+
+
+end
